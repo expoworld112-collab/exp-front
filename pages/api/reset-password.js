47219bd313ee9a729e5b194be85188f429633728
expo <expoworld112@gmail.com>
2025-11-27 22:24:37 +0530

    djkdf

--- pages/api/reset-password.js
+++ pages/api/reset-password.js
@@ -1,6 +1,6 @@
 // pages/api/reset-password.js
 
-import { resetPassword } from "../../controllers/auth.js";
+import { resetPassword } from "../../../Backend-Coding4u-main/controllers/auth.js";
 
 export default async function handler(req, res) {
   // CORS
